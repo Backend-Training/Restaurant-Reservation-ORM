6acc62af37b090ac4ea3109f6f30f74a8bd62908
amrbadran <badran.amr2003@gmail.com>
2025-09-19 11:45:48 +0300

    Add Configuration & fix models

--- RestaurantReservation.Db/Models/MenuItem.cs
+++ RestaurantReservation.Db/Models/MenuItem.cs
@@ -2,7 +2,7 @@ namespace RestaurantReservation.Db.Models;
 
 public class MenuItem
 {
-    public int ItemId { get; set; }
+    public int Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
     public decimal Price { get; set; }
--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -1,5 +1,22 @@
-﻿namespace RestaurantReservation.Db;
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Models;
 
-public class RestaurantReservationDbContext
+namespace RestaurantReservation.Db;
+
+public class RestaurantReservationDbContext : DbContext
 {
+    public DbSet<Customer> Customers { get; set; }
+    public DbSet<Reservation> Reservations { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+    public DbSet<MenuItem> MenuItems { get; set; }
+    public DbSet<Employee> Employees { get; set; }
+    public DbSet<Table> Tables { get; set; }
+    public DbSet<Restaurant> Restaurants { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer(
+            "Server=DESKTOP-7N1E1DT;Database=RestaurantReservationCore;Trusted_Connection=True;TrustServerCertificate=True;MultipleActiveResultSets=true");
+    }
 }
