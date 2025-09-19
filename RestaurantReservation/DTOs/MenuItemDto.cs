4a456e42a8b1d452f61651a7aaa800e7ab426d94
amrbadran <badran.amr2003@gmail.com>
2025-09-19 21:38:34 +0300

    Add DTO: MenuItemDto

---
+++ RestaurantReservation/DTOs/MenuItemDto.cs
@@ -0,0 +1,8 @@
+namespace RestaurantReservation.DTOs;
+
+public class MenuItemDto
+{
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public decimal Price { get; set; }
+}
