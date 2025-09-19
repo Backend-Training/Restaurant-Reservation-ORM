df7d14d35d6373143fb533ea7fc3e7c5e54af6b6
amrbadran <badran.amr2003@gmail.com>
2025-09-19 10:45:06 +0300

    Add Models: Order.cs

---
+++ RestaurantReservation.Db/Models/Order.cs
@@ -0,0 +1,12 @@
+namespace RestaurantReservation.Db.Models;
+
+public class Order
+{
+    public int OrderId { get; set; }
+    public DateTime OrderDate { get; set; }
+    public decimal TotalAmount { get; set; }
+
+    public Reservation Reservation { get; set; }
+    public Employee Employee { get; set; }
+    public ICollection<OrderItem> OrderItems { get; set; }
+}
